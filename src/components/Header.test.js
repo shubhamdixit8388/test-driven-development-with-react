1d233a66ae3956df1b4d3be09eb9693e7e4cea16
Shubham <svdixit8388@gmail.com>
2021-10-07 18:42:43 +0530

    header itms added and tests created for header component element

---
+++ src/components/Header.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, getByTestId } from "@testing-library/react";
+import Header from "./header";
+
+let container = null;
+
+beforeEach(() => {
+  container = render(<Header />).container;
+});
+
+it("should show logo", () => {
+  expect(getByTestId(container, "logo")).toBeTruthy();
+});
+
+it("should show search", () => {
+  expect(getByTestId(container, "search")).toBeTruthy();
+});
+
+it("should show menu", () => {
+  expect(getByTestId(container, "menu")).toBeTruthy();
+});
+
+it("should show filters", () => {
+  expect(getByTestId(container, "home-types")).toBeTruthy();
+  expect(getByTestId(container, "dates")).toBeTruthy();
+  expect(getByTestId(container, "guests")).toBeTruthy();
+  expect(getByTestId(container, "price")).toBeTruthy();
+  expect(getByTestId(container, "rooms")).toBeTruthy();
+  expect(getByTestId(container, "amenities")).toBeTruthy();
+});
