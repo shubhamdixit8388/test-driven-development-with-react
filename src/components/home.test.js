7df3b6833bd32c90bc9c2834972b3c0901aae70f
Shubham <svdixit8388@gmail.com>
2021-10-08 10:24:59 +0530

    onClick test case added

--- src/components/home.test.js
+++ src/components/home.test.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { render, getAllByTestId, act } from "@testing-library/react";
+import {
+  render,
+  getAllByTestId,
+  act,
+  getNodeText,
+} from "@testing-library/react";
 import Homes from "./homes";
 import ApiClient from "../services/api-client";
+import bookingDialogService from "./../services/booking-dialog-service";
 
 let container = null;
 
@@ -46,6 +52,32 @@ it("should show image", () => {
   expect(getAllByTestId(container, "home-image")[0]).toBeTruthy();
 });
 
-it("should show image", () => {
-  expect(getAllByTestId(container, "home-image")[0]).toBeTruthy();
+it("should show home location", () => {
+  const homeLocations = getAllByTestId(container, "home-location");
+  expect(getNodeText(homeLocations[0])).toBe("Test location 1");
+});
+
+it("should show home price", () => {
+  const homePrices = getAllByTestId(container, "home-price");
+  expect(getNodeText(homePrices[0])).toBe("$1/night");
+});
+
+it("should show home booking button", () => {
+  const homeBookingBtn = getAllByTestId(container, "home-booking-btn");
+  expect(homeBookingBtn[0]).toBeTruthy();
+});
+
+it("should open home booking dialog when clicking the button", () => {
+  jest.spyOn(bookingDialogService, "open").mockImplementation(() => {});
+
+  const homeBookingBtn = getAllByTestId(container, "home-booking-btn");
+
+  homeBookingBtn[0].click();
+
+  expect(bookingDialogService.open).toHaveBeenCalledWith({
+    title: "Test home 1",
+    image: "listing.jpg",
+    location: "Test location 1",
+    price: "1",
+  });
 });
--- src/components/homes.jsx
+++ src/components/homes.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ApiClient from "./../services/api-client";
+import bookingDialogService from "./../services/booking-dialog-service";
 
 const Homes = () => {
   const [homes, setHomes] = useState([]);
@@ -12,9 +13,33 @@ const Homes = () => {
     <div>
       {homes.map((home, index) => {
         return (
-          <div data-testid="home" key={index}>
-            <div data-testid="home-title">{home.title}</div>
-            <img src={home.image} data-testid="home-image" />
+          <div className="col-6 col-md-6 col-lg-4 col-xl-3 mb-3" key={index}>
+            <div data-testid="home" className="card w-100">
+              <img
+                data-testid="home-image"
+                src={home.image}
+                alt=""
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <div data-testid="home-title" className="card-title h5">
+                  {home.title}
+                </div>
+                <div data-testid="home-location">{home.location}</div>
+                <div data-testid="home-price">${home.price}/night</div>
+                <div className="d-flex justify-content-end">
+                  <button
+                    data-testid="home-booking-btn"
+                    type="button"
+                    className="btn btn-primary"
+                    className="btn btn-primary"
+                    onClick={() => bookingDialogService.open(home)}
+                  >
+                    Book
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         );
       })}
